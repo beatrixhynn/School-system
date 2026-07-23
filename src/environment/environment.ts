e83b5439bf6007ace404fefe503eb52becf75374
Beatriz Gonçalves <103505461+beatrixhynn@users.noreply.github.com>
2026-07-23 17:15:31 -0300

    Delete src directory

--- src/environment/environment.ts
+++
@@ -1,8 +0,0 @@
-
-        export const environment = {
-            production: false,
-            apiEndpoint: 'http://localhost:8080/school/api',
-            // Outras configurações de ambiente de desenvolvimento uhu
-          };
-
-        
